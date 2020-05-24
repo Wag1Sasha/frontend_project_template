e1569ebad861e306044a70225d314a9032bbe954
lemarco <igor.hoienko@refinitiv.com>
2020-05-24 20:24:30 +0300

    tsConfig additional rules

--- webpack.config.js
+++ webpack.config.js
@@ -49,12 +49,12 @@ const jsLoaders = () => {
 const filenameGen = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
 const cssLoaders = additional => {
     const loaders = [{
-            loader: MiniCSSExtractPlugin.loader,
-            options: {
-                hmr: isDev,
-                reloadAll: true
-            }
-        },
+        loader: MiniCSSExtractPlugin.loader,
+        options: {
+            hmr: isDev,
+            reloadAll: true
+        }
+    },
         'css-loader'
     ]
     if (additional) loaders.push(additional)
@@ -96,60 +96,60 @@ module.exports = {
     devtool: isDev ? 'inline-source-map' : '',
     module: {
         rules: [{
-                test: /\.ts[x]$/,
-                exclude: /node_modules/,
-                use: ['ts-loader']
-            },
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-react']
-                    }
-                }]
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: jsLoaders()
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: cssLoaders()
-            },
-            {
-                test: /\.less$/,
-                exclude: /node_modules/,
-                use: cssLoaders('less-loader')
-            },
-            {
-                test: /\.s[ac]ss$/,
-                exclude: /node_modules/,
-                use: cssLoaders('sass-loader')
-            },
-            {
-                test: /\.[png|svg|jpeg|gif]$}/,
-                exclude: /node_modules/,
-                use: ['file-loader']
-            },
-            {
-                test: /\.[ttf|woff|woff2|eot]$}/,
-                exclude: /node_modules/,
-                use: ['font-loader']
-            },
-            {
-                test: /\.csv$}/,
-                exclude: /node_modules/,
-                use: ['csv-loader']
-            },
-            {
-                test: /\.xml$}/,
-                exclude: /node_modules/,
-                use: ['xml-loader']
-            }
+            test: /\.ts[x]$/,
+            exclude: /node_modules/,
+            use: ['ts-loader']
+        },
+        {
+            test: /\.jsx$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-react']
+                }
+            }]
+        },
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: jsLoaders()
+        },
+        {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: cssLoaders()
+        },
+        {
+            test: /\.less$/,
+            exclude: /node_modules/,
+            use: cssLoaders('less-loader')
+        },
+        {
+            test: /\.s[ac]ss$/,
+            exclude: /node_modules/,
+            use: cssLoaders('sass-loader')
+        },
+        {
+            test: /\.[png|svg|jpeg|gif]$}/,
+            exclude: /node_modules/,
+            use: ['file-loader']
+        },
+        {
+            test: /\.[ttf|woff|woff2|eot]$}/,
+            exclude: /node_modules/,
+            use: ['font-loader']
+        },
+        {
+            test: /\.csv$}/,
+            exclude: /node_modules/,
+            use: ['csv-loader']
+        },
+        {
+            test: /\.xml$}/,
+            exclude: /node_modules/,
+            use: ['xml-loader']
+        }
         ]
     },
     plugins: plugins(),
