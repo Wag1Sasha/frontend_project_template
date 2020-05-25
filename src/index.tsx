bce9e26626ed243ce62e56b451483eebd11d350f
lemarco <igor.hoienko@refinitiv.com>
2020-05-25 13:01:06 +0300

    testing linter with hasky hook

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 function App(): JSX.Element {
-  return <div> Typescript</div>
+  return (
+    <>
+      <div> Typescript</div>
+    </>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
