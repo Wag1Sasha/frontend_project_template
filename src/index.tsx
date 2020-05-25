0125a3644ac7b9ece019b7cb2c85d3678b45190c
lemarco <igor.hoienko@refinitiv.com>
2020-05-25 12:26:07 +0300

    testing jest

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 function App(): JSX.Element {
-  return <div> Typescript</div>
+  return (
+    <>
+      asd,s
+      <div> Typescript</div>
+    </>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
