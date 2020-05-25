5623f9e2cb17474e7a1bdaf33eab746069fa8afb
lemarco <igor.hoienko@refinitiv.com>
2020-05-25 12:26:56 +0300

    testing hasky passed

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 function App(): JSX.Element {
-  return (
-    <>
-      asd,s
-      <div> Typescript</div>
-    </>
-  )
+  return <div> Typescript</div>
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
